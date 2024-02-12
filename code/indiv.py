dd53d6fe496a93592fcfc19cbd80dc66634720ac
cnrde <conrade.tube@gmail.com>
2024-02-12 23:48:40 +0300

    commit

---
+++ code/indiv.py
@@ -0,0 +1,10 @@
+def vn():
+    def fun(a, b):
+        return "Для значений a, b функция f(a,b) = ", a*b/2
+    return fun
+
+if __name__=='__main__':
+    A = float(input("Введите a: "))
+    B = float(input("Введите b: "))
+    f = vn()
+    print(f(A,B))
